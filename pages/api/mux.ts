6330a2d6591127d9cb49990bde26b346ada085a1
Nidhi Kulkarni <nkulkarni@mux.com>
2021-04-20 19:47:43 -0400

    webhook

---
+++ pages/api/mux.ts
@@ -0,0 +1,56 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import Mux from '@mux/mux-node';
+import { buffer } from 'micro';
+
+const webhookSignatureSecret = process.env.MUX_WEBHOOK_SIGNATURE_SECRET;
+
+const verifyWebhookSignature = (rawBody: string | Buffer, req: NextApiRequest) => {
+  if (webhookSignatureSecret) {
+    // this will raise an error if signature is not valid
+    Mux.Webhooks.verifyHeader(rawBody, req.headers['mux-signature'] as string, webhookSignatureSecret);
+  }
+  console.log('Skipping webhook sig verification because no secret is configured'); // eslint-disable-line no-console
+  return true;
+};
+
+//
+// By default, NextJS will look at the content type and intelligently parse the body
+// This is great. Except that for webhooks we need access to the raw body if we want
+// to do signature verification
+//
+// By setting bodyParser: false here we have to extract the rawBody as a string
+// and use JSON.parse on it manually.
+//
+// If we weren't doing webhook signature verification then the code can get a bit simpler
+//
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function muxWebhookHandler (req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  const { method } = req;
+
+  switch (method) {
+    case 'POST': {
+      const rawBody = (await buffer(req)).toString();
+      try {
+        verifyWebhookSignature(rawBody, req);
+      } catch (e) {
+        res.status(400).json({ message: e.message });
+        return;
+      }
+      const jsonBody = JSON.parse(rawBody);
+      const { data, type } = jsonBody;
+
+      if (type !== 'video.asset.ready') {
+        res.json({ message: 'thanks Mux' });
+        return;
+      }
+      break;
+    } default:
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
