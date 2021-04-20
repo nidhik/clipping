af39119b1ae7a1d3b577d785049e337d69f65753
Nidhi Kulkarni <nkulkarni@mux.com>
2021-04-20 18:08:14 -0400

    copy clips from: https://github.com/muxinc/stream.new/compare/dj/create-clipping-flow?expand=1

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  console.log(process.env.MUX_TOKEN_ID)
-  console.log(process.env.MUX_TOKEN_SECRET)
   res.status(200).json({ name: 'John Doe' })
 }
