6fe48458bd838f6de13963ada0e859725b93bfff
Nidhi Kulkarni <nkulkarni@mux.com>
2021-04-22 15:35:42 -0400

    clip last 30 sec or a specified start and end

--- pages/api/clips.tsx
+++ pages/api/clips.tsx
@@ -1,17 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import got from 'got';
 
-async function createAssetClip ({ playbackId, startTime, endTime }) {
+async function clip ({ assetId, startTime, endTime }) {
   const headers = {
     Authorization: 'Basic ' + Buffer.from(`${process.env.MUX_TOKEN_ID}:${process.env.MUX_TOKEN_SECRET}`).toString('base64'),
     'content-type': 'application/json',
   };
-  const playbackResp = await got.get(`https://api.mux.com/video/v1/playback-ids/${playbackId}`, { headers, responseType: 'json' });
-
-  let responseBody:any = playbackResp.body;
-  if (responseBody?.data?.object?.type === 'asset') {
-    const assetId = responseBody?.data.object.id;
-    const newAssetResp = await got.post(`https://api.mux.com/video/v1/assets`, {
+  const newAssetResp = await got.post(`https://api.mux.com/video/v1/assets`, {
       json: {
         input: [
           {
@@ -28,25 +23,59 @@ async function createAssetClip ({ playbackId, startTime, endTime }) {
     });
     let newAssetResponseBody:any = newAssetResp.body
     return newAssetResponseBody.data;
+}
+
+async function clipFromEnd ({ assetId, seconds: length }) {
+  const headers = {
+    Authorization: 'Basic ' + Buffer.from(`${process.env.MUX_TOKEN_ID}:${process.env.MUX_TOKEN_SECRET}`).toString('base64'),
+    'content-type': 'application/json',
+  };
+  const playbackResp = await got.get(`https://api.mux.com/video/v1/assets/${assetId}`, { headers, responseType: 'json' });
+
+  let responseBody:any = playbackResp.body;
+  console.log(responseBody.data.duration)
+  if (responseBody?.data.duration) {
+    
+    const assetDuration = responseBody?.data.duration;
+    const newAssetResp = await got.post(`https://api.mux.com/video/v1/assets`, {
+      json: {
+        input: [
+          {
+            url: `mux://assets/${assetId}`,
+            start_time: assetDuration - length,
+          }
+        ],
+        playback_policy: 'public',
+        mp4_support: 'standard',
+      },
+      headers,
+      responseType: 'json',
+    });
+    let newAssetResponseBody:any = newAssetResp.body
+    return newAssetResponseBody.data;
   } else {
-    throw new Error('Not an asset playback ID, not clipping this');
+    throw new Error('Not an asset ID, not clipping this ' + assetId);
   }
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   const { method, body } = req;
-  const { playbackId, startTime, endTime } = body;
+  const { assetId, startTime, endTime} = body;
   let asset;
 
   switch (method) {
     case 'POST':
-      if (!playbackId) {
+      if (!assetId) {
         // format this error the way the frontend expects
-        res.json({ errors: ['Need a playbackId']});
+        res.json({ errors: ['Need an assetId']});
         return;
       }
       try {
-        asset = await createAssetClip({ playbackId, startTime, endTime });
+        if (!startTime) {
+          asset = await clipFromEnd({ assetId, seconds: 30 });
+        } else {
+          asset = await clip({ assetId, startTime, endTime });
+        }
         res.json({ id: asset.id });
       } catch (e) {
         if (e.response && e.response.body) {
